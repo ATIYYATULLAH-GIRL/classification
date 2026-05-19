6e1752991bd0b4f164a042d55ed654817a492184
ATIYYATULLAH-GIRL <mabas2014.am@gmail.com>
2026-05-19 17:40:20 +0100

    Add files via upload

---
+++ ication.py
@@ -0,0 +1,19 @@
+from numpy import where
+from collections import Counter
+from sklearn.datasets import make_blobs
+from matplotlib import pyplot
+
+x,y=make_blobs(n_samples=1000,centers=3,random_state=1)
+print(x.shape)
+
+counter=Counter(y)
+print(counter)
+
+for i in range(10):
+    print(x[i],y[i])
+
+for label, _ in counter.items():
+    row_x=where(y==label)[0]
+    pyplot.scatter(x[row_x,0], x[row_x,1], label=str(label))
+pyplot.legend()
+pyplot.show()
